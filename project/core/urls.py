6cb89f44f973ed75ae4ab67ff9e3e6f4e4ac1568
Alex Zayets <a.l.e.x.s.c.r.a.t.666@gmail.com>
2015-05-23 21:06:10 +0300

    fix

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,20 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'profit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', views.HomeView.as_view(), name='home'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('accounts.urls', namespace="accounts", app_name="accounts")),
-    url(r'^transactions/', include('transactions.urls', namespace="transactions", app_name="transactions")),
 ]
 
 # rest api
-
-urlpatterns += [
-    url(r'^api/transactions/', include('transactions.api.urls', namespace="api_transactions", app_name="transactions")),
-]
+#
+#urlpatterns += [    
+#]
